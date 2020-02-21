c0def0deca4913ec09b525e1e2cb19ed55df8a64
jphrubant <jphrubant@gmail.com>
2020-02-21 09:32:53 +0100

    first commit with basic structure

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/third', function(req, res, next) {
+  res.render('third', { title: 'Express' });
+});
+
+
+router.get('/second', function(req, res, next) {
+  res.render('second', { title: 'Express' });
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
 
 module.exports = router;
