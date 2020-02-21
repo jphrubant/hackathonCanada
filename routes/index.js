38142bcee4927e52f7f2eb8b6c6620c1cf049717
jphrubant <jphrubant@gmail.com>
2020-02-21 13:11:52 +0100

    all routing set up

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,115 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
-/* Get Years page. */
-router.get('/years', function(req, res, next) {
-  res.render('years');
+/* Get Atheletes page */
+router.get('/athletes', function(req, res, next) {
+  axios.get('https://canadajson.herokuapp.com/athletes')
+    .then( (response) => {
+      res.render('index', {athletes:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+
+  axios.get('https://canadajson.herokuapp.com/sports')
+    .then( (response) => {
+      res.render('index', {sports:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+
+    axios.get('https://canadajson.herokuapp.com/years')
+    .then( (response) => {
+      res.render('index', {years:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  res.render('athletes');
 });
 
 /* Get Sports page */
 router.get('/sports', function(req, res, next) {
+  axios.get('https://canadajson.herokuapp.com/athletes')
+    .then( (response) => {
+      res.render('index', {athletes:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+
+  axios.get('https://canadajson.herokuapp.com/sports')
+    .then( (response) => {
+      res.render('index', {sports:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+
+    axios.get('https://canadajson.herokuapp.com/years')
+    .then( (response) => {
+      res.render('index', {years:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
   res.render('sports');
 });
 
-/* Get Atheletes page */
-router.get('/athletes', function(req, res, next) {
-  res.render('athletes');
+/* Get Years page. */
+router.get('/years', function(req, res, next) {
+  axios.get('https://canadajson.herokuapp.com/athletes')
+    .then( (response) => {
+      res.render('index', {athletes:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+
+  axios.get('https://canadajson.herokuapp.com/sports')
+    .then( (response) => {
+      res.render('index', {sports:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+
+    axios.get('https://canadajson.herokuapp.com/years')
+    .then( (response) => {
+      res.render('index', {years:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  res.render('years');
 });
 
 //*Get Homepage */
 router.get('/', function(req, res, next) {
-  axios.get('https://canadajson.herokuapp.com/sports')
+  axios.get('https://canadajson.herokuapp.com/athletes')
     .then( (response) => {
-      res.render('index', {data:response.data});
+      res.render('index', {athletes:response.data});
     })
     .catch((err) => {
       console.log(err)
+    });
+
+  axios.get('https://canadajson.herokuapp.com/sports')
+    .then( (response) => {
+      res.render('index', {sports:response.data});
     })
+    .catch((err) => {
+      console.log(err)
+    });
+
+    axios.get('https://canadajson.herokuapp.com/years')
+    .then( (response) => {
+      res.render('index', {years:response.data});
+    })
+    .catch((err) => {
+      console.log(err)
+    });
 });
 
 
